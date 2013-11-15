cc99d1706a4a7ecc26f8ff57dc816218648c2055
Garrett Jacobson <gjacobso@yelp.com>
2013-11-15 10:35:52 -0800

    Updated lines metrics to include more file types

--- git_code_debt/metrics/common.py
+++ git_code_debt/metrics/common.py
@@ -1,8 +1,53 @@
 
+PYTHON = 'Python'
+YAML = 'Yaml'
+TEMPLATE = 'Template'
+CSS = 'Css'
+MAKO_TEMPLATE = 'Mako_Template'
+JAVASCRIPT = 'Javascript'
+JAVA = 'Java'
+ILLUSTRATOR = 'Illustrator'
+HTML = 'Html'
+CCPP = 'C/C++'
+TEXT = 'Text'
+SQL = 'SQL'
+
+
 # Maps a set of file extensions to a nice name.
 # Updating this will cause that file type to be tracked for LinesOfCode metric.
 FILE_TYPE_MAP = {
-    '.py': 'Python',
-    '.yaml': 'Yaml',
-    '.tmpl': 'Template',
+    '.py': PYTHON,
+
+    '.yaml': YAML,
+    '.yml': YAML,
+
+    '.css': CSS,
+    '.scss': CSS,
+
+    '.tmpl': TEMPLATE,
+
+    '.mako': MAKO_TEMPLATE,
+
+    '.js': JAVASCRIPT,
+
+    '.java': JAVA,
+
+    '.ai': ILLUSTRATOR,
+
+    '.htm': HTML,
+    '.html': HTML,
+
+    '.h': CCPP,
+    '.c': CCPP,
+    '.cpp': CCPP,
+
+    '.md': TEXT,
+    '.rst': TEXT,
+    '.csv': TEXT,
+    '.log': TEXT,
+    '.json': TEXT,
+    '.xml': TEXT,
+    '.txt': TEXT,
+
+    '.sql': SQL,
 }
