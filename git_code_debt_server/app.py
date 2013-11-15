f799935380e4875ce21d858e0fbd28aab8c388f6
Cameron Paul <cpaul37@gmail.com>
2013-11-14 16:07:29 -0800

    Added db connection

--- git_code_debt_server/app.py
+++ git_code_debt_server/app.py
@@ -1,5 +1,7 @@
 import flask
+import sqlite3
 
+from git_code_debt_server.config import DATABASE_PATH
 from git_code_debt_server.servlets.graph import graph
 from git_code_debt_server.servlets.index import index
 
@@ -7,6 +9,14 @@ app = flask.Flask(__name__)
 
 app.register_blueprint(index)
 app.register_blueprint(graph)
+
+@app.before_request
+def before_request():
+    flask.g.db = sqlite3.connect(DATABASE_PATH)
+
+@app.teardown_request
+def teardown_request(exception):
+    flask.g.db.close()
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
---
+++ git_code_debt_server/config.py
@@ -0,0 +1,3 @@
+import os.path
+
+DATABASE_PATH = os.path.abspath('git_code_debt_server/database.db')
