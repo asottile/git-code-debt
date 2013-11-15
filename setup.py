ee3117e596fdbbefc7d8b2a55bd69d093796adff
Anthony Sottile <asottile@umich.edu>
2013-11-15 12:29:32 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Code Debt Metrics',
-    version='0.1.1',
+    version='0.1.2',
     packages=[
        'git_code_debt',
        'git_code_debt.metrics',
