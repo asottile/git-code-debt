ead58144a6feb4315b48c1e47968dbcbb6443017
Anthony Sottile <asottile@umich.edu>
2013-11-19 19:02:19 -0800

    Smarter setup.

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name='Code Debt Metrics',
     version='0.1.3',
-    packages=[
-       'git_code_debt',
-       'git_code_debt.metrics',
-       'git_code_debt_server',
-       'git_code_debt_server.logic',
-       'git_code_debt_server.servlets',
-       'git_code_debt_util',
-    ],
+    packages=find_packages('.', exclude=('tests*', 'testing*')),
     install_requires=[
         'flask',
         'mako',
