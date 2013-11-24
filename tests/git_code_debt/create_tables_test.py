1590445d209fdcffef2de22d62f9cf99dbad3d88
Anthony Sottile <asottile@umich.edu>
2013-11-23 19:22:33 -0800

    Added smoke test for populating metric ids.

--- tests/git_code_debt/create_tables_test.py
+++ tests/git_code_debt/create_tables_test.py
@@ -4,7 +4,9 @@ import sqlite3
 import testify as T
 
 from git_code_debt.create_tables import create_schema
+from git_code_debt.create_tables import get_metric_ids
 from git_code_debt.create_tables import get_modules
+from git_code_debt.create_tables import populate_metric_ids
 from testing.base_classes.temp_dir_test_case import TempDirTestCase
 
 @T.suite('integration')
@@ -24,6 +26,22 @@ class TestCreateSchema(TempDirTestCase):
             T.assert_in('metric_names', table_names)
             T.assert_in('metric_data', table_names)
 
+@T.suite('integration')
+class TestPopulateMetricIds(TempDirTestCase):
+
+    def test_populate_metric_ids(self):
+        db_path = os.path.join(self.temp_dir, 'db.db')
+
+        with sqlite3.connect(db_path) as db:
+            create_schema(db)
+            populate_metric_ids(db, tuple(), False)
+
+            results = db.execute('SELECT * FROM metric_names')
+            # Smoke test assertion
+            T.assert_length(
+                results,
+                len(get_metric_ids(tuple(), True)),
+            )
 
 class TestGetModules(T.TestCase):
 
