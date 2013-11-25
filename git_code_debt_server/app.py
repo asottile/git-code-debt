25a962abfb18d5203d11806a63f15aa7ede20499
Anthony Sottile <asottile@umich.edu>
2013-11-25 07:58:21 -0800

    Custom port for server.

--- git_code_debt_server/app.py
+++ git_code_debt_server/app.py
@@ -1,6 +1,6 @@
+import argparse
 import flask
 import sqlite3
-import sys
 
 from git_code_debt_server.servlets.graph import graph
 from git_code_debt_server.servlets.index import index
@@ -21,5 +21,9 @@ def teardown_request(exception):
     flask.g.db.close()
 
 if __name__ == '__main__':
-    database_path = sys.argv[1]
-    app.run('0.0.0.0', debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('database_path', type=str)
+    parser.add_argument('--port', type=int, default=5000)
+    args = parser.parse_args()
+    database_path = args.database_path
+    app.run('0.0.0.0', port=args.port, debug=True)
