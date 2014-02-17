1667e4c28d969af615d028a4b828cc2c868957bc
Anthony Sottile <asottile@umich.edu>
2014-02-16 18:41:17 -0800

    Add integration test for --color never.

--- tests/git_code_debt/list_metrics_test.py
+++ tests/git_code_debt/list_metrics_test.py
@@ -10,12 +10,25 @@ from git_code_debt.list_metrics import main
 from git_code_debt.list_metrics import NORMAL
 
 
-@pytest.mark.integration
-def test_list_metricssmoke():
-    # This test is just to make sure that it doesn't fail catastrophically
+@pytest.yield_fixture
+def print_mock():
     with mock.patch.object(__builtin__, 'print', autospec=True) as print_mock:
-        main([])
-        assert print_mock.called
+        yield print_mock
+
+
+@pytest.mark.integration
+def test_list_metrics_smoke(print_mock):
+    # This test is just to make sure that it doesn't fail catastrophically
+    main([])
+    assert print_mock.called
+
+
+@pytest.mark.integration
+def test_list_metrics_no_color_smoke(print_mock):
+    main(['--color', 'never'])
+    assert all([
+        '\033' not in call[0][0] for call in print_mock.call_args_list
+    ])
 
 
 def test_color_no_color():
