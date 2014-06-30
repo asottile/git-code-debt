60422842dd260fa6874163355a96ee1f8b5b652f
Anthony Sottile <asottile@umich.edu>
2014-06-30 09:19:04 -0700

    Remove cached_property.

--- testing/utilities/response.py
+++ testing/utilities/response.py
@@ -2,8 +2,6 @@
 import pyquery
 import simplejson
 
-from git_code_debt_util.decorators import cached_property
-
 
 class Response(object):
     """A Response wraps a response from a testing Client."""
@@ -11,10 +9,10 @@ class Response(object):
     def __init__(self, response):
         self.response = response
 
-    @cached_property
+    @property
     def pq(self):
         return pyquery.PyQuery(self.response.data)
 
-    @cached_property
+    @property
     def json(self):
         return simplejson.loads(self.response.data)
