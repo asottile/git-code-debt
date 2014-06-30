6faee1608801bc4a969739fa9e360766187bb99d
Anthony Sottile <asottile@umich.edu>
2014-06-30 10:42:59 -0700

    Rename package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='Code Debt Metrics',
+    name='git_code_debt',
     version='0.3.4',
     packages=find_packages('.', exclude=('tests*', 'testing*')),
     package_data={
