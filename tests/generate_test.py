1e7ba5486e3f873a229c37d0cbc0dc24c2f98718
Anthony Sottile <asottile@umich.edu>
2014-07-13 16:34:09 -0700

    Add "failing" test for #10.

--- tests/generate_test.py
+++ tests/generate_test.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import collections
+import os
+import pytest
+import sqlite3
 
 from git_code_debt.generate import increment_metric_values
 from git_code_debt.generate import main
@@ -54,3 +57,27 @@ def test_generate_new_data_created(sandbox, cloneable):
     main([cloneable, sandbox.db_path])
     after_data_count = get_metric_data_count(sandbox)
     assert after_data_count > before_data_count
+
+
+def test_regression_for_issue_10(sandbox, cloneable):
+    # Create a commit, then create another commit at a previous time
+    with cwd(cloneable):
+        cmd_output(
+            'git', 'commit', '--allow-empty', '-m', 'c1',
+            env=dict(
+                os.environ, GIT_COMMITTER_DATE='Wed, Feb 16 14:00 2011 +0100',
+            ),
+        )
+        cmd_output(
+            'git', 'commit', '--allow-empty', '-m', 'c2',
+            env=dict(
+                os.environ, GIT_COMMITTER_DATE='Tue, Feb 15 14:00 2011 +0100',
+            ),
+        )
+
+    main([cloneable, sandbox.db_path])
+    data_count_before = get_metric_data_count(sandbox)
+    with pytest.raises(sqlite3.IntegrityError):
+        main([cloneable, sandbox.db_path])
+    data_count_after = get_metric_data_count(sandbox)
+    assert data_count_before == data_count_after
