0b9f0a2e0dfa9a0d0feb8777637897cfb5cfd6f8
Anthony Sottile <asottile@umich.edu>
2014-07-14 08:38:08 -0700

    Add get_all_data function.

--- git_code_debt/server/logic.py
+++ git_code_debt/server/logic.py
@@ -13,24 +13,15 @@ Metric = collections.namedtuple('Metric', ['name', 'value', 'date'])
 
 def get_metric_ids_from_database():
     result = flask.g.db.execute(
-        '''
-        SELECT
-            name
-        FROM metric_names
-        ORDER BY name
-        '''
+        'SELECT name FROM metric_names ORDER BY name'
     ).fetchall()
     return [name for name, in result]
 
 
 def get_latest_sha():
-    result = flask.g.db.execute('''
-        SELECT
-            sha
-        FROM metric_data
-        ORDER BY timestamp DESC
-        LIMIT 1
-    ''').fetchone()
+    result = flask.g.db.execute(
+        'SELECT sha FROM metric_data ORDER BY timestamp DESC LIMIT 1'
+    ).fetchone()
 
     # If there is no data result will be None
     return result[0] if result else None
@@ -38,15 +29,15 @@ def get_latest_sha():
 
 def get_sha_for_date(date):
     result = flask.g.db.execute(
-        '''
-        SELECT
-            sha
-        FROM metric_data
-        WHERE
-            timestamp <= ?
-        ORDER BY timestamp DESC
-        LIMIT 1
-        ''',
+        '\n'.join((
+            'SELECT',
+            '    sha',
+            'FROM metric_data',
+            'WHERE',
+            '    timestamp <= ?',
+            'ORDER BY timestamp DESC',
+            'LIMIT 1',
+        )),
         [date],
     ).fetchone()
 
@@ -60,16 +51,16 @@ def get_metrics_for_sha(sha):
         return collections.defaultdict(int)
 
     result = flask.g.db.execute(
-        '''
-        SELECT
-            metric_names.name,
-            metric_data.running_value
-        FROM metric_data
-        INNER JOIN metric_names ON
-            metric_names.id = metric_data.metric_id
-        WHERE
-            metric_data.sha = ?
-        ''',
+        '\n'.join((
+            'SELECT',
+            '    metric_names.name,',
+            '    metric_data.running_value',
+            'FROM metric_data',
+            'INNER JOIN metric_names ON',
+            '    metric_names.id = metric_data.metric_id',
+            'WHERE',
+            '    metric_data.sha = ?',
+        )),
         [sha],
     ).fetchall()
 
@@ -79,19 +70,19 @@ def get_metrics_for_sha(sha):
 def metrics_for_dates(metric_name, dates):
     def get_metric_for_timestamp(timestamp):
         result = flask.g.db.execute(
-            '''
-            SELECT
-                running_value,
-                timestamp
-            FROM metric_data
-            INNER JOIN metric_names ON
-                metric_names.id == metric_data.metric_id
-            WHERE
-                metric_names.name == ? AND
-                timestamp < ?
-            ORDER BY timestamp DESC
-            LIMIT 1
-            ''',
+            '\n'.join((
+                'SELECT',
+                '    running_value,',
+                '    timestamp',
+                'FROM metric_data',
+                'INNER JOIN metric_names ON',
+                '    metric_names.id == metric_data.metric_id',
+                'WHERE',
+                '    metric_names.name == ? AND',
+                '    timestamp < ?',
+                'ORDER BY timestamp DESC',
+                'LIMIT 1',
+            )),
             [metric_name, timestamp],
         ).fetchone()
         if result:
@@ -105,26 +96,27 @@ def metrics_for_dates(metric_name, dates):
 def get_first_data_timestamp(metric_name, db=None):
     db = db or flask.g.db
     result = db.execute(
-        '''
-        SELECT metric_data.timestamp
-        FROM metric_data
-        INNER JOIN metric_names ON metric_names.id = metric_data.metric_id
-        WHERE
-            metric_names.name = ? AND
-            metric_data.timestamp < (
-                SELECT metric_data.timestamp
-                FROM metric_data
-                INNER JOIN metric_names
-                    ON metric_names.id = metric_data.metric_id
-                WHERE
-                    metric_names.name = ? AND
-                    metric_data.running_value > 0
-                ORDER BY metric_data.timestamp
-                LIMIT 1
-            )
-        ORDER BY metric_data.timestamp DESC
-        LIMIT 1;
-        ''',
+        '\n'.join((
+            'SELECT metric_data.timestamp',
+            'FROM metric_data',
+            'INNER JOIN metric_names ON'
+            '    metric_names.id = metric_data.metric_id',
+            'WHERE',
+            '    metric_names.name = ? AND',
+            '    metric_data.timestamp < (',
+            '        SELECT metric_data.timestamp',
+            '        FROM metric_data',
+            '        INNER JOIN metric_names',
+            '            ON metric_names.id = metric_data.metric_id',
+            '        WHERE',
+            '            metric_names.name = ? AND',
+            '            metric_data.running_value > 0',
+            '        ORDER BY metric_data.timestamp',
+            '        LIMIT 1',
+            '    )',
+            'ORDER BY metric_data.timestamp DESC',
+            'LIMIT 1',
+        )),
         [metric_name, metric_name],
     ).fetchone()
 
@@ -133,13 +125,37 @@ def get_first_data_timestamp(metric_name, db=None):
     else:
         # Otherwise return the first timestamp in the db (or 0 for no data)
         return db.execute(
-            '''
-            SELECT
-                IFNULL(MIN(metric_data.timestamp), 0)
-            FROM metric_data
-            INNER JOIN metric_names ON metric_names.id = metric_data.metric_id
-            WHERE
-                metric_names.name = ?
-            ''',
+            '\n'.join((
+                'SELECT',
+                '    IFNULL(MIN(metric_data.timestamp), 0)',
+                'FROM metric_data',
+                'INNER JOIN metric_names ON',
+                '    metric_names.id = metric_data.metric_id',
+                'WHERE',
+                '    metric_names.name = ?',
+            )),
             [metric_name]
         ).fetchone()[0]
+
+
+def get_all_data(metric_name, min_timestamp, max_timestamp, db=None):
+    db = db or flask.db
+    result = db.execute(
+        '\n'.join((
+            'SELECT',
+            '    metric_data.sha,',
+            '    metric_data.running_value,',
+            '    metric_data.timestamp',
+            'FROM metric_data',
+            'INNER JOIN metric_names ON',
+            '    metric_names.id == metric_data.metric_id',
+            'WHERE',
+            '    metric_names.name == ? AND',
+            '    timestamp >= ? AND',
+            '    timestamp < ?',
+            'ORDER BY TIMESTAMP ASC',
+        )),
+        [metric_name, min_timestamp, max_timestamp],
+    ).fetchall()
+
+    return tuple(result)
--- tests/server/logic_test.py
+++ tests/server/logic_test.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from git_code_debt.logic import get_metric_mapping
 from git_code_debt.logic import insert_metric_values
 from git_code_debt.repo_parser import Commit
+from git_code_debt.server.logic import get_all_data
 from git_code_debt.server.logic import get_first_data_timestamp
 
 
@@ -34,3 +35,51 @@ def test_some_data_returns_last_zero_before_data(sandbox):
         insert(db, '2' * 40, 2222, 0)
         insert(db, '3' * 40, 3333, 1)
         assert get_first_data_timestamp('PythonImportCount', db=db) == 2222
+
+
+def _insert_all_data_test_data(db):
+    insert(db, '1' * 40, 1111, 1)
+    insert(db, '2' * 40, 2222, 2)
+    insert(db, '3' * 40, 3333, 3)
+
+
+def test_get_all_data_inclusive(sandbox):
+    with sandbox.db() as db:
+        _insert_all_data_test_data(db)
+        ret = get_all_data('PythonImportCount', 0, 4444, db=db)
+
+    assert ret == (
+        ('1' * 40, 1, 1111),
+        ('2' * 40, 2, 2222),
+        ('3' * 40, 3, 3333),
+    )
+
+
+def test_get_all_data_edge_left(sandbox):
+    with sandbox.db() as db:
+        _insert_all_data_test_data(db)
+        ret = get_all_data('PythonImportCount', 1111, 4444, db=db)
+
+    assert ret == (
+        ('1' * 40, 1, 1111),
+        ('2' * 40, 2, 2222),
+        ('3' * 40, 3, 3333),
+    )
+
+
+def test_get_all_data_edge_right(sandbox):
+    with sandbox.db() as db:
+        _insert_all_data_test_data(db)
+        ret = get_all_data('PythonImportCount', 0, 3333, db=db)
+
+    assert ret == (
+        ('1' * 40, 1, 1111),
+        ('2' * 40, 2, 2222),
+    )
+
+
+def test_get_all_data_empty_set(sandbox):
+    with sandbox.db() as db:
+        ret = get_all_data('PythonImportCount', 0, 1, db=db)
+
+    assert ret == ()
