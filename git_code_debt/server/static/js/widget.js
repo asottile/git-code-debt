7edd1cd61b96067867aba3a2bf3630f8f1019e5c
Anthony Sottile <asottile@umich.edu>
2014-11-07 08:32:13 -0800

    Make widget work for multiple chunks of diff.

--- git_code_debt/server/static/js/widget.js
+++ git_code_debt/server/static/js/widget.js
@@ -1,38 +1,53 @@
 $(function () {
-    $('[data-git-code-debt-domain]').each(function (i, el) {
-        var element = $(el),
-            debtDomain = element.data('git-code-debt-domain'),
-            metricNames = Array.prototype.map.bind(element.find('ul li'))(
-                function (li) {
-                    return $(li).text();
-                }
-            ),
-            diff = element.find('div').text(),
-            iframe = $('<iframe>');
+    var idToElements = {},
+        thisScript = $('script[src$="/static/js/widget.js"]'),
+        debtDomain = thisScript[0].src.split('/static/js/widget.js')[0],
+        iframe = $('<iframe>'),
+        idToElement = {};
 
-        $(window).on('message', function (e) {
-            var originalEvent = e.originalEvent;
-            if (originalEvent.data.ready) {
+    $(window).on('message', function (e) {
+        var targetElement,
+            originalEvent = e.originalEvent;
+
+        if (originalEvent.data.ready) {
+            $('.git-code-debt').each(function (elementId, el) {
+                var element = $(el),
+                    metricNames = Array.prototype.map.call(
+                        element.find('ul li'),
+                        function (li) {
+                            return $(li).text();
+                        }
+                    ),
+                    diff = element.find('div').text();
+
+                idToElement[elementId] = element;
+
                 iframe[0].contentWindow.postMessage(
                     {
+                        elementId: elementId,
                         metricNames: metricNames,
                         diff: diff
                     },
                     '*'
                 );
-            } else if (originalEvent.data.metrics) {
-                iframe.remove();
-                element.html($(originalEvent.data.metrics)).show();
-            }
-        });
+            });
+        } else if (originalEvent.data.metrics) {
+            targetElement = idToElement[originalEvent.data.elementId];
+            delete idToElement[originalEvent.data.elementId];
+            targetElement.html($(originalEvent.data.metrics)).show();
 
-        iframe.css({
-            border: 0,
-            width: 1,
-            height: 1,
-            position: 'absolute',
-            left: -9999
-        }).attr('src', debtDomain + '/widget/frame');
-        $('body').append(iframe);
+            if ($.isEmptyObject(idToElement)) {
+                iframe.remove();
+            }
+        }
     });
+
+    iframe.css({
+        border: 0,
+        width: 1,
+        height: 1,
+        position: 'absolute',
+        left: -9999
+    }).attr('src', debtDomain + '/widget/frame');
+    $('body').append(iframe);
 });
